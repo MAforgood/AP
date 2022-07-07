7050f1750497ee8e624e31d9487ab94c15185f96
Parham-Hd <108261400+Parham-Hd@users.noreply.github.com>
2022-07-08 01:06:15 +0430

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Collections.ObjectModel;
 using Microsoft.Data.SqlClient;
 using System.IO;
 using System.Data;
@@ -28,14 +29,29 @@ namespace project
     {
         public MainWindow()
         {
+            Payment payment = new Payment(20);
             InitializeComponent();
-            Payment payment = new Payment();
-            UserLogin userLogin = new UserLogin();
-            User user = new User("", "", "", "", "");
-            UserPage userPage = new UserPage(user);
-            userPage.Show();
-            //userLogin.Show();
-            //payment.Show();
+            payment.Show();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            UserLogin userlog = new UserLogin();
+            userlog.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Admin a = new Admin();
+            a.Show();
+            this.Close();
+        }
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            UserSignup usersignup = new UserSignup();
+            usersignup.Show();
+            this.Close();
         }
     }
     public enum Type
@@ -44,15 +60,17 @@ namespace project
     }
     public class User
     {
+        public static List<User> users = new List<User>(); 
         public string First_Name { get; set; }
         public string Last_Name { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-        public double Wallet { get; set; }
+        public float Wallet { get; set; }
         public string Password { get; set; }
-        public Type type = Type.normal;
-        public List<Book> ShoppingCart = new List<Book>();
-        public List<Book> Books = new List<Book>();
+        public Type type { get; set; }
+        public ObservableCollection<Book> ShoppingCart = new ObservableCollection<Book>();
+        public ObservableCollection<Book> BookMarks = new ObservableCollection<Book>();
+        public ObservableCollection<Book> Books = new ObservableCollection<Book>();
         public User(string fname, string lname, string pnum, string email, string pass)
         {
             First_Name = fname;
@@ -60,6 +78,7 @@ namespace project
             Phone = pnum;
             Email = email;
             Password = pass;
+            type = Type.normal;
         }
     }
     public class Card
@@ -68,7 +87,7 @@ namespace project
         public int CVV { get; set; }
         public string Password { get; set; }
         public DateOnly Expiration { get; set; }
-        public Card(string id,int cvv,string pass,DateOnly date)
+        public Card(string id, int cvv, string pass, DateOnly date)
         {
             Id = id;
             CVV = cvv;
@@ -117,21 +136,44 @@ namespace project
             else return false;
         }
     }
-    public class Book : IEnumerable
+    public class Book
     {
-        public string name { get; set; }
-        public string Writer { get; set; }
-        public IEnumerator GetEnumerator()
+        public static List<Book> books = new List<Book>();
+        public int id { get;}
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public float Price { get; set; }
+        public int Published_Year { get; set; }
+        public string Summary { get; set; }
+        public string Cover_Path { get; set; }
+        public string Pdf_Path { get; set; }
+        public float discount_value { get; set; }
+        public TimeOnly discount_Time { get; set; }
+        public ObservableCollection<int> Ratings = new ObservableCollection<int>();
+        public float Rate { get; set; }
+        public int Total_Sale { get; set; }
+        public float Total_Income { get; set; }
+        public Type type { get; set; }
+        public Book(string _name,string _author,float _price,int _published,string _summary,string cover,string pdf)
         {
-            foreach (var item in this)
+            if (books.Count == 0)
             {
-                yield return item;
+                id = 0;
             }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return this.GetEnumerator();
+            else
+            id = books[books.Count - 1].id + 1;
+            Name = _name;
+            Author = _author;
+            Price = _price;
+            Published_Year = _published;
+            Summary = _summary;
+            Cover_Path = cover;
+            Pdf_Path = pdf;
+            type = Type.normal;
+            if (Ratings != null)
+            {
+                Rate = Ratings.Sum(x => x/Ratings.Count);
+            }
         }
     }
     class Management
@@ -140,16 +182,41 @@ namespace project
     }
     static class ExtentionMetodes
     {
-        public static List<Book> search(this IEnumerable<Book> data,string _name)
-        {
-            List<Book> list = data.Where(x => x.name.Contains(_name)||x.Writer.Contains(_name)).Select(x => x).ToList();
-            return list;
-        }
+        //public static List<Book> search(this IEnumerable<Book> data,string _name)
+        //{
+        //    List<Book> list = data.Where(x => x.name.Contains(_name)||x.Writer.Contains(_name)).Select(x => x).ToList();
+        //    return list;
+        //}
         public static void AddTotable(this User user)
+        {
+            float wallet = 0;
+            string Books = "";
+            foreach (Book book in user.Books)
+            {
+                Books += book.id.ToString()+",";
+            }
+            string BookMarks = "";
+            foreach (Book book in user.BookMarks)
+            {
+                BookMarks += book.id.ToString() + ",";
+            }
+            string Cart = "";
+            foreach (Book book in user.ShoppingCart)
+            {
+                Cart += book.id.ToString() + ",";
+            }
+            SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\programms\c#\project\Database.mdf;Integrated Security=True;Connect Timeout=30");
+            connection.Open();
+            string Command = "insert into Users values('" + user.Email.Trim() + "','" + user.First_Name.Trim() + "','" + user.Last_Name.Trim() + "','" + user.Phone.Trim() + "','" + user.Password.Trim() + "','" + user.Wallet + "','"+Books+ "','"+BookMarks+"','"+0+"','"+Cart+"')";
+            SqlCommand cmd = new SqlCommand(Command, connection);
+            cmd.ExecuteNonQueryAsync();
+            connection.Close();
+        }
+        public static void AddTotable(this Book book)
         {
             SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\programms\c#\project\DataSql\data.mdf;Integrated Security=True;Connect Timeout=30");
             connection.Open();
-            string Command = "insert into Users values('"+user.Email.Trim()+"','"+user.First_Name.Trim()+"','"+user.Last_Name.Trim()+"','"+user.Phone.Trim()+",'"+0+"'')";
+            string Command = "insert into Users(Name,Author,Published Year,Price,Summary,Cover,Pdf Path,Type,vipfee) values('" + book.Name.Trim() + "','" + book.Author.Trim() + "','" + book.Published_Year + "','" + book.Price + " ','" + book.Summary.Trim() + "','" + book.Cover_Path.Trim() + "','" + book.Pdf_Path.Trim() + "','" + "normal" + "','" + 0 + "'";
             SqlCommand cmd = new SqlCommand(Command, connection);
             cmd.BeginExecuteNonQuery();
             connection.Close();
